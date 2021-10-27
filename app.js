f22ee478e05ca2f2359f710dcf5d981200ca43ee
lcq <1005428477@qq.com>
2021-10-27 21:53:33 +0800

    tutorial7/Routing & Multiple Views

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-angular.module("demoApp", []).controller("PhoneListCtrl", function ($scope) {
-
-    $scope.phones = [
-        { name: 'Nexus S', snippet: 'Fast just got faster with Nexus S.', age: 3 },
-        { name: 'Motorola XOOM™ with Wi-Fi', snippet: 'The Next, Next Generation tablet.', age: 2 },
-        { name: 'MOTOROLA XOOM™', snippet: 'The Next, Next Generation tablet.', age: 1 }
-    ];
-    console.log($scope);
+var app = angular.module("demoApp", ["ngRoute"]);
+app.config(["$routeProvider", function (routeProvider) {
+    routeProvider
+        .when("/phones", {
+            templateUrl: "partials/phone-list.html"
+        })
+        .when("/phones/:id", {
+            templateUrl: "partials/phone-detail.html"
+        })
+        .otherwise({
+            redirectTo: '/phones'
+        })
+}]);
+app.controller("PhoneListCtrl", function ($scope, $http) {
+    $http.get("phones.json").then(function (resp) {
+        $scope.phones = resp.data;
+    });
+    // console.log($scope);
+}).controller("PhoneDetailCtrl", function ($scope, $routeParams) {
+    $scope.phoneId = $routeParams.id;
 });
