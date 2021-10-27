7c23772dfff351237671f86e289df8a2ea66ed4c
liuchangqing <1005428477@qq.com>
2021-10-27 17:52:56 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 angular.module("demoApp", []).controller("PhoneListCtrl", function ($scope) {
 
     $scope.phones = [
-        { name: 'Nexus S', snippet: 'Fast just got faster with Nexus S.' },
-        { name: 'Motorola XOOM™ with Wi-Fi', snippet: 'The Next, Next Generation tablet.' },
-        { name: 'MOTOROLA XOOM™', snippet: 'The Next, Next Generation tablet.' }
-    ]
+        { name: 'Nexus S', snippet: 'Fast just got faster with Nexus S.', age: 3 },
+        { name: 'Motorola XOOM™ with Wi-Fi', snippet: 'The Next, Next Generation tablet.', age: 2 },
+        { name: 'MOTOROLA XOOM™', snippet: 'The Next, Next Generation tablet.', age: 1 }
+    ];
+    console.log($scope);
 });
